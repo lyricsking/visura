005c3ea0b33160a60f6d342a5036f9aac5e3b2e7
lyricsking <asaajay775@gmail.com>
2024-08-27 13:25:37 +0100

    update tips.model.ts

--- app/Blog/models/tips.model.ts
+++ app/Blog/models/tips.model.ts
@@ -3,13 +3,27 @@ import { ITips, Country } from "../types/tips.type";
 
 export type TipsModelType = Model<ITips>;
 
+const predictionSchema = new Schema<IPrediction>({
+  type: {
+    type: [String],
+    enum: ["outcome", "scoreline"],
+    required: true,
+  },
+  value: { type: String, required: true },
+  reason: { type: String, required: true },
+});
+
 // Define the schema for the Tips model
 const tipsSchema = new Schema<ITips, TipsModelType>(
   {
     teamA: { type: String, required: true },
     teamB: { type: String, required: true },
     matchDate: { type: Date, required: true },
-    country: { type: String, enum: Object.keys(Country), required: true },
+    country: { 
+      type: String, 
+      enum: Object.keys(Country), 
+      required: true 
+    },
     league: {
       type: String,
       enum: Object.values(Country).flat(),
@@ -18,8 +32,8 @@ const tipsSchema = new Schema<ITips, TipsModelType>(
     teamARank: { type: Number, required: true },
     teamBRank: { type: Number, required: true },
     author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    prediction: { type: [predictionSchema], required: true },
     introduction: { type: String, required: true },
-    prediction: { type: String, required: true },
     excerpt: { type: String, required: true },
     featuredImage: { type: String, required: true },
     tags: { type: [String], required: true },
