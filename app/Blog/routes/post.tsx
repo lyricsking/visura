f198b7d194a99d368d2d909455c756677a069e96
Jamiu Adeniyi <asaajay775@gmail.com>
2024-08-28 14:07:54 +0000

    Update: Improve blog post page to properly style markdown content

--- app/Blog/routes/post.tsx
+++ app/Blog/routes/post.tsx
@@ -8,37 +8,39 @@ export default function Post() {
   const { post } = useLoaderData<typeof loader>();
 
   return (
-    <div className="px-4 md:py-8 md:px-6">
-      <div className="max-w-3xl mx-auto">
-        <div className="p-4">
-          <p className="text-gray-500">Published on April 9, 2023</p>
-          <h1 className="text-5xl font-bold mt-1">{post.title}</h1>
+    <article className="max-w-3xl mx-auto px-4 md:py-8 md:px-6">
+      <div className="p-4">
+        <p className="text-gray-500">Published on April 9, 2023</p>
+        <h1 className="text-5xl font-bold mt-1">{post.title}</h1>
 
-          <div className="flex items-center mt-4">
-            <img
-              src="author-image-url"
-              alt=" author"
-              className="w-12 h-12 rounded-full"
-            />
-            <div>
-              <div className="ml-3">
-                <p className="font-semibold">Jamiu</p>
-                <p className="text-gray-500">@jamiu</p>
-              </div>
+        <div className="flex items-center mt-4">
+          <img
+            src="author-image-url"
+            alt=" author"
+            className="w-12 h-12 rounded-full"
+          />
+          <div>
+            <div className="ml-3">
+              <p className="font-semibold">Jamiu</p>
+              <p className="text-gray-500">@jamiu</p>
             </div>
           </div>
         </div>
-
-        <div className="p-4">
-          <img
-            src={post.featuredImage}
-            alt={post.title}
-            className="w-full h-auto border rounded-lg mb-6"
-          />
-          <div className="" /> {/* Formatted content here */}
-        </div>
       </div>
-    </div>
+
+      <div className="p-4">
+        <img
+          src={post.featuredImage}
+          alt={post.title}
+          className="w-full h-auto border rounded-lg mb-6"
+        />
+        {/* Formatted content here */}
+        <div
+          className="prose md:prose-lg lg:prose-xl"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+      </div>
+    </article>
   );
 }
 
