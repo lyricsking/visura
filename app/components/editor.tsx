9c9e9766465f83b104766a196b62ac5c9cee3e91
lyricsking <asaajay775@gmail.com>
2024-09-01 13:41:27 +0100

    update

--- app/components/editor.tsx
+++ app/components/editor.tsx
@@ -41,33 +41,134 @@ export interface MarkdownEditorProps extends TextareaProps {
 
 export function MarkdownEditor(props: MarkdownEditorProps) {
   const { editorRef, ...attrs } = props;
-
+  
+  const [searchParams, setSearchParams] = useSearchParams();
+  
+  const togglePreview = () => {
+    setSearchParams((prev) => {
+      
+      prev.has("preview") 
+      ? prev.delete("preview") 
+      : prev.set("preview", true);
+      
+      return prev;
+    });
+  };
+  
   return (
     <div className="flex rounded-md w-full max-w-lg mx-auto border bg-gray-100 divide-y">
       <div className="w-full">
-        {/* Toolbar */}
-        <Toolbar textareaRef={editorRef} />
-        {/* EditableContent Div */}
+        <div className="flex items-center h-16 gap-2 px-2 divide-x border-black overflow-hidden">
+          {/* Toolbar */}
+          <Toolbar textareaRef={editorRef} />
+          {/* Preview toggle */}
+          <div className="ml-auto ">
+            <Button
+              variant="ghost"
+              size="default"
+              onClick={togglePreview}
+            >
+              Preview
+            </Button>
+          </div>
+        </div>
+        {/* EditableContent Div and Preview */}
         <div className="mx-1 mb-1">
+          {/***/}
           <Textarea
             ref={editorRef}
             className="min-h-44 w-full border-t-2 bg-white p-2 outline-none"
             {...attrs}
           />
+          { /* Preview */ }
+          <div className="min-h-44 w-full border-t-2 bg-white p-2">
+            <ReactMarkdown>{customMarkdownParser(editorRef.current.value)}</ReactMarkdown>
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-export function Toolbar({
-  textareaRef,
-}: {
+type ToolbarItem = {
+  prefix: string,
+  suffix?: string,
+  icon: React.ElementType
+}
+
+const toolbarItems: Record<string, ToolbarItem> = {
+  bold: {
+    prefix: "**",
+    suffix: "**",
+    icon: BoldIcon
+  },
+  italic: {
+    prefix: "*",
+    suffix: "*",
+    icon: ItalicIcon,
+  }, 
+  strikethrough: {
+    prefix:"~~",
+    suffix: "~~",
+    icon: StrikethroughIcon,
+  },
+  h1: {
+    prefix:"# ",
+    icon: Heading1Icon,
+  },
+  h1: {
+    prefix: "## ",
+    icon: Heading2Icon
+  },
+  h3: {
+    prefix: "### ",
+    icon: Heading3Icon
+  },
+  link: {
+    prefix: "[ link text ] (link url)",
+    icon: LinkIcon
+  },
+  quote: {
+    prefix: "> ",
+    icon: QuoteIcon
+  },
+  image: {
+    prefix: "![alt text](image url)",
+    icon: ImageIcon
+  },
+  numberList: {
+    prefix: "1. Item",
+    icon: OrderedIcon
+  },
+  list: {
+    prefix: "## ",
+    icon: ListIcon
+  },
+  leftAlign: {
+    prefix: "<< ",
+    suffix: " <<",
+    icon: AlignLeftIcon
+  },
+  rightAlign: {
+    prefix: ">> ",
+    suffix: " >>",
+    icon: AlignRightIcon
+  },
+  justify: {
+    prefix: "== ",
+    suffix: " ==",
+    icon: AlignJustifyIcon
+  }
+} as const
+
+type ToolbarProps = {
   textareaRef: MutableRefObject<HTMLTextAreaElement | null>;
-}) {
-  const insertMarkdown = (prefix: string, shouldWrap: boolean = false) => {
-    let suffix = "";
-    if (shouldWrap) suffix = prefix;
+  itemsKey: (keyof typeof toolbarItems)[]
+}
+
+export function Toolbar({ textareaRef, itemsKey }: ToolbarProps) {
+  //  Todo Memoize to avoid re-initializing for each re-render.
+  const insertMarkdown = (prefix: string, suffix: string = "") => {
 
     if (!textareaRef.current) return;
 
@@ -94,172 +195,26 @@ export function Toolbar({
   };
 
   return (
-    <div className="flex items-center gap-2 px-2 divide-x border-black overflow-hidden">
       <div className="flex items-center gap-2 divide-x border-black overflow-x-auto">
-        <Button
-          variant="ghost"
-          size="icon"
-          className="shrink-0 grow-0 font-bold"
-          onMouseDown={(e) => {
-            e.preventDefault(); // Prevent the form losing focus
-            return insertMarkdown("**", true);
-          }}
-        >
-          <BoldIcon className="w-5 h-5" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="italic"
-          onMouseDown={(e) => {
-            e.preventDefault(); // Prevent the form losing focus
-            return insertMarkdown("__", true);
-          }}
-        >
-          <ItalicIcon className="w-5 h-5" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="line "
-          onMouseDown={(e) => {
-            e.preventDefault(); // Prevent the form losing focus
-            return insertMarkdown("~~", true);
-          }}
-        >
-          <StrikethroughIcon className="w-5 h-5" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="line "
-          onMouseDown={(e) => {
-            e.preventDefault(); // Prevent the form losing focus
-            return insertMarkdown("# ");
-          }}
-        >
-          <Heading1Icon className="w-5 h-5" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="line "
-          onMouseDown={(e) => {
-            e.preventDefault(); // Prevent the form losing focus
-            return insertMarkdown("## ");
-          }}
-        >
-          <Heading2Icon className="w-5 h-5" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="line "
-          onMouseDown={(e) => {
-            e.preventDefault(); // Prevent the form losing focus
-            return insertMarkdown("### ");
-          }}
-        >
-          <Heading3Icon className="w-5 h-5" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="line "
-          onMouseDown={(e) => {
-            e.preventDefault(); // Prevent the form losing focus
-            return insertMarkdown("### ");
-          }}
-        >
-          <ImageIcon className="w-5 h-5" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="line "
-          onMouseDown={(e) => {
-            e.preventDefault(); // Prevent the form losing focus
-            return insertMarkdown("### ");
-          }}
-        >
-          <QuoteIcon className="w-5 h-5" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="line "
-          onMouseDown={(e) => {
-            e.preventDefault(); // Prevent the form losing focus
-            return insertMarkdown("### ");
-          }}
-        >
-          <LinkIcon className="w-5 h-5" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="line "
-          onMouseDown={(e) => {
-            e.preventDefault(); // Prevent the form losing focus
-            return insertMarkdown("### ");
-          }}
-        >
-          <ListIcon className="w-5 h-5" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="line "
-          onMouseDown={(e) => {
-            e.preventDefault(); // Prevent the form losing focus
-            return insertMarkdown("### ");
-          }}
-        >
-          <ListOrderedIcon className="w-5 h-5" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="line "
-          onMouseDown={(e) => {
-            e.preventDefault(); // Prevent the form losing focus
-            return insertMarkdown("### ");
-          }}
-        >
-          <AlignLeftIcon className="w-5 h-5" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="line "
-          onMouseDown={(e) => {
-            e.preventDefault(); // Prevent the form losing focus
-            return insertMarkdown("### ");
-          }}
-        >
-          <AlignJustifyIcon className="w-5 h-5" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="line "
-          onMouseDown={(e) => {
-            e.preventDefault(); // Prevent the form losing focus
-            return insertMarkdown("### ");
-          }}
-        >
-          <AlignRightIcon className="w-5 h-5" />
-        </Button>
+        {
+          itemsKey.map((itemKey) => {
+            let item = toolbarItems[itemKey];
+            const IconTag = item.icon;
+            
+            return (
+              <Button
+                variant="ghost"
+                size="icon"
+                onMouseDown={(e) => {
+                  e.preventDefault(); // Prevent the form from losing focus
+                  return insertMarkdown(item.prefix, item.suffix);
+                }}
+              >
+                <IconTag className="w-5 h-5" />
+              </Button>
+            )
+          })
+        }
       </div>
-      <div className="ml-auto ">
-        <Button
-          variant="ghost"
-          size="default"
-          onClick={() => alert("Preview not implemented yet")}
-        >
-          Preview
-        </Button>
-      </div>
-    </div>
   );
 }
---
+++ app/utils/md-helper.ts
@@ -0,0 +1,6 @@
+export function customMarkdownParser(content) {
+    return content
+    .replace(/<< (.*?) <</g, '<div style="text-align: left;">$1</div>')
+    .replace(/>> (.*?) >>/g, '<div style="text-align: right;">$1</div>')
+    .replace(/== (.*?) ==/g, '<div style="text-align: justify;">$1</div>');
+}
