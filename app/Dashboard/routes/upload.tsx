3782806a3e1b772dd7338c654dc4b439d44b6d3d
Jamiu Adeniyi <asaajay775@gmail.com>
2024-09-04 04:24:46 -0800

    Added file upload route

---
+++ app/Dashboard/routes/upload.tsx
@@ -0,0 +1,47 @@
+import {
+  json,
+  NodeOnDiskFile,
+  redirect,
+  unstable_composeUploadHandlers,
+  unstable_createFileUploadHandler,
+  unstable_createMemoryUploadHandler,
+  unstable_parseMultipartFormData,
+  type ActionFunctionArgs,
+} from "@remix-run/node";
+export async function action({ request }: ActionFunctionArgs) {
+  let folder = "post";
+  const fileUploaderHandler = unstable_createFileUploadHandler({
+    filter({ contentType }) {
+      return contentType.includes("image");
+    },
+    // strore the images in images/${folder} folder
+    directory: `./public/images/${folder}`,
+    // By default a number is added to duplicate files,
+    // we will disable it to replace it
+    avoidFileConflicts: false,
+    // Use the the actual file name as the final filename
+    file({ filename }) {
+      return filename;
+    },
+    // Limit the max size to 10MB
+    maxPartSize: 10 * 1024 * 1024,
+  });
+
+  let composedHandler = unstable_composeUploadHandlers(
+    fileUploaderHandler,
+    unstable_createMemoryUploadHandler()
+  );
+
+  let formData = await unstable_parseMultipartFormData(
+    request,
+    composedHandler
+  );
+
+  let files = formData.getAll("file") as NodeOnDiskFile[];
+  return json({
+    files: files.map((file) => ({
+      name: file.name,
+      url: `/images/${folder}/${file.name}`,
+    })),
+  });
+}
