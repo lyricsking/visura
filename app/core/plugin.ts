debde6b4565b610ca7485a56e5ceb66bce775001
Jamiu Adeniyi <30894401+lyricsking@users.noreply.github.com>
2024-09-18 07:53:50 +0000

    Update

--- app/core/plugin.ts
+++ app/core/plugin.ts
@@ -7,19 +7,3 @@ export interface IPlugin {
   defaultConfig: PluginSettingsSchema;
   registerRoutes: (path: string, defineRoute: DefineRouteFunction) => any; // This could register routes
 }
-
-export const plugins: { [pluginName: string]: IPlugin } = {};
-
-export const registerPlugin = (plugin: IPlugin) => {
-  if (!plugin.name) {
-    throw new Error("Plugin must have a unique name");
-  }
-
-  if (plugins[plugin.name]) {
-    throw new Error(
-      `Cannot register a duplicate plugin: ${plugin.name}. Plugin name must be unique.`
-    );
-  }
-
-  plugins[plugin.name] = plugin;
-};
