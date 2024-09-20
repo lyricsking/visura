7fd23ef8ee8821d999ee793846ee6845e82f0c87
Jamiu Adeniyi <30894401+lyricsking@users.noreply.github.com>
2024-09-20 08:03:21 +0000

    Update

--- app/plugins/blog/routes/tip.tsx
+++ app/plugins/blog/routes/tip.tsx
@@ -4,18 +4,11 @@ import { findPostById, findPostBySlug } from "../server/post.server";
 import ReactMarkdown from "react-markdown";
 import { formatDateByParts, formatDateOrTime } from "~/utils/date";
 import { findTipBySlug } from "../server/tips.server";
-import { Card, CardContent, CardFooter } from "~/components/card";
+import { Card, CardContent, CardFooter, CardHeader } from "~/components/card";
 
 export default function TipPage() {
   const { tip } = useLoaderData<typeof loader>();
-  let title =
-    tip.leagueCountry +
-    " " +
-    tip.league +
-    ": " +
-    tip.teamA +
-    " vs " +
-    tip.teamB;
+  let title = tip.teamA + " - " + tip.teamB;
 
   let publishedOn = tip.publishedOn
     ? formatDateOrTime(new Date(tip.publishedOn), {
@@ -25,7 +18,15 @@ export default function TipPage() {
       })
     : null;
 
-  const matchDate = formatDateByParts(new Date(tip.matchDate));
+  //const matchDate = formatDateByParts(new Date(tip.matchDate));
+  const matchDate = formatDateOrTime(new Date(tip.matchDate), {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
 
   return (
     <article className="w-full mx-auto max-w-3xl px-4 md:py-8 md:px-6">
@@ -49,8 +50,18 @@ export default function TipPage() {
       </div>
 
       <div className="p-4">
-        <Card className="w-full bg-white/90 overflow-hidden mb-6">
-          <CardContent className="px-2">
+        <Card className="w-full bg-gray-50/90 shadow-md overflow-hidden mb-6">
+          <CardHeader className="justify-center border-b py-4">
+            <div className="w-full grid grid-cols-[30%_1fr] gap-4 font-semibold">
+              Match Date:
+              <span className="w-full font-normal">{matchDate}</span>
+            </div>
+
+            <div className="text-xl font-bold text-center">
+              {tip.leagueCountry} - {tip.league}
+            </div>
+          </CardHeader>
+          <CardContent className="py-6 px-2">
             <div className="grid grid-cols-[30%_1fr_30%] gap-4 items-center md:gap-6 text-center mb-6">
               <p className="font-semibold">{tip.teamA}</p>
               <div className="grid grid-cols-1 font-semibold gap-4">
@@ -76,12 +87,6 @@ export default function TipPage() {
               <p className="font-semibold">{tip.teamB}</p>
             </div>
           </CardContent>
-          <CardFooter className="justify-center border-t p-4">
-            <div className="font-semibold">
-              
-              Match Date <span className="font-normal">{matchDate}</span>
-            </div>
-          </CardFooter>
         </Card>
         {/* Formatted content here */}
         <div className="prose md:prose-lg lg:prose-xl mb-6">
@@ -91,20 +96,30 @@ export default function TipPage() {
 
         <div className="mb-6">
           <h4 className="text-2xl font-semibold">Match Predictions</h4>
-          <blockquote className="p-4 my-4 border-s-4 border-gray-300 bg-gray-50">
-            <p className="text-xl italic font-medium leading-relaxed text-gray-900 dark:text-white">
-              "Flowbite is just awesome. It contains tons of predesigned
-              components and pages starting from login screen to complex
-              dashboard. Perfect choice for your next SaaS application."
-            </p>
-          </blockquote>
+          <div className="grid gap-6">
+            {Object.keys(tip.prediction).map((key) => {
+              return (
+                <blockquote className="p-4 my-4 border-s-4 border-gray-300 bg-gray-50 rounded-md">
+                  <p className="text-2xl capitalize italic font-semibold text-gray-900 dark:text-white mb-6">
+                    {key}:
+                  </p>
+
+                  <p className="text-xl italic font-medium leading-relaxed text-gray-900 dark:text-white">
+                    "Flowbite is just awesome. It contains tons of predesigned
+                    components and pages starting from login screen to complex
+                    dashboard. Perfect choice for your next SaaS application."
+                  </p>
+                </blockquote>
+              );
+            })}
+          </div>
         </div>
       </div>
     </article>
   );
 }
 
-export const loade<r = async ({ params }: LoaderFunctionArgs) => {
+export const loader = async ({ params }: LoaderFunctionArgs) => {
   let slug = params["slug"];
   if (!slug) throw Error("Tip id must be provided.");
 
