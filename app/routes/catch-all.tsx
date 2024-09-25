f490b128bda414b821073f2141344b2356a469d5
lyricsking <asaajay775@gmail.com>
2024-09-25 12:38:10 +0100

    update catch-all.tsx

--- app/routes/catch-all.tsx
+++ app/routes/catch-all.tsx
@@ -1,29 +1,40 @@
 import { LoaderFunction } from "@remix-run/node";
 import { match } from "path-to-regexp";
 
-// Example of registered plugin paths
-const pluginRoutes = {
-  "blog": "/blog/posts/:postId"
-};
-
 export let loader: LoaderFunction = async ({ params, request }) => {
   const url = new URL(request.url);
   const path = url.pathname; // e.g., "/blog/posts/first-post"
   
+  const pluginRoutes = findRoute("app");
+  
+  if(pluginRoutes){
   // Try matching the URL with the registered plugin paths
-  for (let key in pluginRoutes) {
-    const route = pluginRoutes[key];
-    const matchRoute = match(route, { decode: decodeURIComponent });
+  for (let route in pluginRoutes) {
+    const matchRoute = match(route.path, { decode: decodeURIComponent });
     const matchResult = matchRoute(path);
 
     if (matchResult) {
-      const { params } = matchResult;
+      const { path, params } = matchResult;
       // Do something with the matched params
       // e.g., load the post based on postId
-      return { matched: true, params };
+      const data = route.loader(args);
+      return { path: route.path, data: data, params };
     }
-  }
-
+  }}
+  
   // If no route matched, return 404
-  throw new Response("Not Found", { status: 404 });
+  //throw new Response("Not Found", { status: 404 });
+  // Return default path
+  return { path: "not-found", data: {}};
+  
 };
+
+
+export default function CatchAll() {
+  const {path, data, params} = useLoaderData<typeof loader>();
+  
+  const route = findRoute("app", path);
+  const Component = route.component;
+  
+  return <Component data={data}/>
+}
