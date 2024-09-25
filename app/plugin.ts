f4403aa556f21534c17b8bbde1f2c90e76f645bd
lyricsking <asaajay775@gmail.com>
2024-09-25 01:17:40 +0100

    Created plugin interface

--- app/plugin.ts
+++ app/plugin.ts
@@ -1,11 +1,47 @@
-import { DefineRouteFunction } from "@remix-run/dev/dist/config/routes";
-import { PluginSettingsType } from "~/config";
-import { Menu } from "~/utils/menu";
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-export interface IPlugin<Configs extends PluginSettingsType = any> {
+export interface IPlugin {
   name: string;
   description: string;
   version: string;
-  defaultConfig: Configs;
-  registerRoutes: (defineRoute: DefineRouteFunction, config: Configs) => any; // This could register routes
+  settings?: Record<string, any>;
+  onInit?: () => void;
+  onDestroy?: () => void;
+  layoutComponent?: React.ComponentType;
 }
+
+const plugins: { [key: string]: IPlugin } = {};
+
+const loadPlugins = () => {
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    // const pluginDir = path.join(__dirname, 'plugins');
+    const pluginDir = __dirname;
+    // Read the plugins directory synchronously
+    const pluginFolders = fs.readdirSync(pluginDir);
+
+    // Filter and load only directories containing an index.ts file
+    const loadedPlugins = pluginFolders.map((pluginFolder) => {
+      const pluginPath = path.join(pluginDir, pluginFolder, 'index.ts');
+      try {
+        // Synchronously load the plugin using dynamic import
+        const plugin = require(pluginPath).default;
+        return plugin;
+      } catch (err) {
+        console.error(`Error loading plugin from ${pluginPath}:`, err);
+        return null;
+      }
+    });
+
+    // Return only successfully loaded plugins
+    return loadedPlugins.filter(Boolean);
+  } catch (err) {
+    console.error('Error loading plugins:', err);
+    return [];
+  }
+};
+
+export default loadPlugins;
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import routes from "./app/route";
+import esbuild from "esbuild";
 
 installGlobals();
 
 export default defineConfig({
   server: { port: 3000 },
   plugins: [
-    tsconfigPaths(),
     remix({
       ignoredRouteFiles: ["**/.*"],
       // appDirectory: "app",
@@ -19,7 +19,24 @@ export default defineConfig({
       routes(defineRoutes) {
         return defineRoutes(routes);
       },
+      buildEnd: async () => {
+        await esbuild.build({
+          alias: { "~": "./app" },
+          outfile: "build/server/index.js",
+          entryPoints: ["server/index.ts"],
+          external: ['./build/server/*'],
+          platform: 'node',
+          format: 'esm',
+          packages: 'external',
+          bundle: true,
+          logLevel: 'info',
+        }).catch((error: unknown) => {
+          console.error('Error building server:', error);
+          process.exit(1);
+        });
+      }
     }),
+    tsconfigPaths()
   ],
   optimizeDeps: {
     exclude: ["@mapbox"], // Mapbox lib causing error during build time.
