30dbf2c76b417d1e5ee8e16aa6ff6b2e4fa2cbf2
Jamiu Adeniyi <asaajay775@gmail.com>
2024-10-23 04:15:05 -0800

    Update

---
+++ server/index.ts
@@ -0,0 +1,32 @@
+import { createRequestHandler, remixFastify } from "@mcansh/remix-fastify";
+import { fastify } from "fastify";
+import sourceMapSupport from "source-map-support";
+import getPort, { portNumbers } from "get-port";
+import chalk from "chalk";
+
+sourceMapSupport.install();
+
+const app = fastify();
+await app.register(remixFastify);
+
+const host = process.env.HOST || "127.0.0.1";
+const desiredPort = Number(process.env.PORT) || 3000;
+const portToUse = await getPort({
+  port: portNumbers(desiredPort, desiredPort + 100),
+});
+
+let address = await app.listen({
+  port: portToUse,
+  host,
+});
+let { port: usedPort } = new URL(address);
+
+if (usedPort !== String(desiredPort)) {
+  console.warn(
+    chalk.yellow(
+      `Port ${desiredPort} is not available, using ${usedPort} instead.`
+    )
+  );
+}
+
+console.log(chalk.green(`app ready: ${address}`));
