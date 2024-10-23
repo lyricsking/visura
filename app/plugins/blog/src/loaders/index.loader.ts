5c312217ed55a431de8e653a7273fec39e95ebf5
Jamiu Adeniyi <asaajay775@gmail.com>
2024-10-23 09:00:28 -0800

    Update

--- app/plugins/blog/src/loaders/index.loader.ts
+++ app/plugins/blog/src/loaders/index.loader.ts
@@ -1,4 +1,4 @@
-import {  } from "vite-env-only";
+import { serverOnly$ } from "vite-env-only";
 import { PluginLoaderFunction } from "~/core/types/route";
 import { findPosts } from "../server/post.server";
 import { findTips } from "../server/tips.server";
