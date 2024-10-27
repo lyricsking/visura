95fd8d8a7aec3ec56e0a89064bb70fef43ca544b
Jamiu Adeniyi <asaajay775@gmail.com>
2024-10-26 21:13:49 -0800

    Update catch-all

--- app/core/public/routes/catch-all.tsx
+++ app/core/public/routes/catch-all.tsx
@@ -1,81 +1,49 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { match } from "path-to-regexp";
-import NotFound from "./not-found";
 import { getAppContext } from "~/app";
+import { MatchResult } from "path-to-regexp";
+import { PageContentType } from "~/core/types/page";
+import { renderPage } from "~/core/components/ui/render-page";
 
 const NOT_FOUND_PATH = "not-found";
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
+export const loader = async (args: LoaderFunctionArgs) => {
+  const { request } = args;
   const path = new URL(request.url).pathname;
   const app = await getAppContext();
 
-  const matchedRoute = app.routes?.find(route => {
+  const matchedRoute = app.routes?.find((route) => {
     const matchRoute = match(route.path, { decode: decodeURIComponent });
     return matchRoute(path);
   });
 
   if (matchedRoute) {
-    const matchResult = match(matchedRoute.path, { decode: decodeURIComponent })(path);
-    const data = matchedRoute.loader ? await matchedRoute.loader({ request, app }) : null;
+    const { path: p, params } = match(matchedRoute.path, {
+      decode: decodeURIComponent,
+    })(path) as MatchResult<any>;
+    const data = matchedRoute.loader
+      ? await matchedRoute.loader({ ...args, app })
+      : null;
 
     return json({
       path: matchedRoute.path,
-      params: matchResult.params,
+      params: params,
       data,
       content: matchedRoute.content,
     });
   }
 
   // If no route matched, return default not-found response
-  return json({ path: NOT_FOUND_PATH, data: {} });
-};
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const path = new URL(request.url).pathname;
-  const app = await getAppContext();
-
-  const matchedRoute = app.routes?.find(route => {
-    const matchRoute = match(route.path, { decode: decodeURIComponent });
-    return matchRoute(path);
+  return json({
+    data: {},
+    path: NOT_FOUND_PATH,
+    params: undefined,
+    content: null,
   });
-
-  if (matchedRoute) {
-    const matchResult = match(matchedRoute.path, { decode: decodeURIComponent })(path);
-    const data = matchedRoute.loader ? await matchedRoute.loader({ request, app }) : null;
-
-    return json({
-      path: matchedRoute.path,
-      params: matchResult.params,
-      data,
-      content: matchedRoute.content,
-    });
-  }
-
-  // If no route matched, return default not-found response
-  return json({ path: NOT_FOUND_PATH, data: {} });
 };
-
 
 export default function CatchAll() {
-  const { path } = useLoaderData<typeof loader>();
-
-  // useEffect(() => {
-  //   alert(componentPath);
-  // }, []);
-
-  // return <div dangerouslySetInnerHTML={{ __html: componentPath }} />;
-  // if (componentPath && pathname !== NOT_FOUND_PATH) {
-  //   // Use React.lazy to dynamically import the component
-  //   const DynamicComponent = React.lazy(
-  //     () => import(/* @vite-ignore */ `/app/${componentPath}`)
-  //   );
-
-  //   return (
-  //     <Suspense fallback={<div>Loading component...</div>}>
-  //       <DynamicComponent {...data} pathname={pathname} />
-  //     </Suspense>
-  //   );
-  // }
-
-  return <NotFound />;
+  const { data, path, content } = useLoaderData<typeof loader>();
+  return renderPage(path, content as PageContentType, data);
 }
