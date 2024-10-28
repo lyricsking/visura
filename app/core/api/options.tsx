1695e6f860929b0664bbf01d5b2ada1ce49613d4
Jamiu Adeniyi <asaajay775@gmail.com>
2024-10-27 20:18:12 -0800

    Improvement: Improve options and plugins fetch request

--- app/core/api/options.tsx
+++ app/core/api/options.tsx
@@ -2,6 +2,14 @@ import { json, LoaderFunctionArgs } from "@remix-run/node";
 import { OptionModel } from "../models/option.model";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const options = await OptionModel.find({});
+  const url = new URL(request.url);
+
+  const name = url.searchParams.get("name");
+
+  const query: { name?: string } = {};
+
+  if (name) query.name = name;
+  const options = await OptionModel.find(query);
+
   return json({ options });
 };
--- app/core/api/plugins.tsx
+++ app/core/api/plugins.tsx
@@ -2,8 +2,14 @@ import { json, LoaderFunctionArgs } from "@remix-run/node";
 import { PluginModel } from "../models/plugin.model";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const query = new URL(request.url).searchParams;
+  const url = new URL(request.url);
 
-  const plugins = await PluginModel.find({});
+  const isActive = url.searchParams.get("isActive") === "true";
+
+  const query: { isActive?: boolean } = {};
+
+  if (isActive) query.isActive = isActive;
+
+  const plugins = await PluginModel.find(query);
   return json({ plugins });
 };
