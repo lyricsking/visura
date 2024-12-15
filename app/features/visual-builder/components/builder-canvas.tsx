155002c09fffbdf252eeb3773036a92f6119fcb4
Jamiu Adeniyi <asaajay775@gmail.com>
2024-12-15 02:09:20 -0900

    Update

--- app/features/visual-builder/components/builder-canvas.tsx
+++ app/features/visual-builder/components/builder-canvas.tsx
@@ -1,12 +1,16 @@
 import {
   Box,
   Button,
+  ButtonGroup,
   Center,
   Container,
+  Divider,
   Flex,
   NavLink,
   ScrollArea,
+  SegmentedControl,
   Stack,
+  Text,
 } from "@mantine/core";
 import { useVisualBuilder } from "./visual-builder.provider";
 import { cn } from "~/shared/utils/util";
@@ -18,32 +22,65 @@ export default function ComponentsCanvas() {
   const handleClick = (id: string | undefined) => id && setSelection(id);
 
   return (
-    <Container h={"calc(100vh - 117px)"} w={"100%"} p="0">
-      <div className="flex justify-center gap-4 py-4 h-[50px] bg-gray-200">
-        <Button size="compact-xs" variant="outline">
-          Desktop
-        </Button>
-        <Button size="compact-xs" variant="outline">
-          Mobile
-        </Button>
+    <Container h={"calc(100vh - 112px)"} w={"100%"} p="0">
+      <div className="flex items-center justify-center gap-4 px-4 h-[40px] bg-gray-200">
+        <div className="flex items-center gap-2 ml-auto">
+          <Text size="xs" fw={"bold"}>
+            Preview:{" "}
+          </Text>
+          <SegmentedControl
+            defaultValue={"desktop"}
+            size="xs"
+            data={[
+              { label: "Desktop", value: "desktop" },
+              { label: "Mobile", value: "mobile" },
+            ]}
+          />
+        </div>
+
+        <Divider
+          orientation="vertical"
+          mx="-xs"
+          color="#d1d1d1"
+          size={"xs"}
+          variant="solid"
+        />
+
+        <Button
+          size="compact-sm"
+          color="#228be6"
+          // color="#cccccc"
+          children="Save"
+        />
       </div>
       {/* Check if we have an active component for editing
       and display appropriate settings component */}
-      {components && components.length > 0 && (
+      {components && (
         <ScrollArea
           h={"100%"}
-          my="sm"
           bg="white"
           className={cn("m-4 border rounded-sm")}
         >
-          {components.map((component) => (
-            <div
-              key={component.props.id}
-              onClick={() => handleClick(component.props.id)}
-            >
-              <component.component {...component.props} />
-            </div>
-          ))}
+          {components.length > 0 ? (
+            components.map((component) => (
+              <div
+                key={component.props.id}
+                onClick={() => handleClick(component.props.id)}
+              >
+                <component.component {...component.props} />
+              </div>
+            ))
+          ) : (
+            <Center h={"calc(100vh - 120px)"} w="100%">
+              <Text
+                c={"dimmed"}
+                children="Select a component button on the left sidebar to start editing your page."
+                size="xl"
+                ta={"center"}
+                mx={"xl"}
+              />
+            </Center>
+          )}
         </ScrollArea>
       )}
     </Container>
