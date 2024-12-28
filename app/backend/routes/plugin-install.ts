bd76239af4a689d9f6a36f609d245599e15911fa
Jamiu Adeniyi <asaajay775@gmail.com>
2024-12-28 06:54:59 -0900

    Upadet

--- app/backend/routes/plugin-install.ts
+++ app/backend/routes/plugin-install.ts
@@ -1,5 +1,19 @@
+import { fi } from "@faker-js/faker";
+import { ActionFunctionArgs } from "@remix-run/node";
+import path from "path";
+
 export const loader = () => {
   return Response.json({ message: "Ready to install plugin." });
 };
 
-export const action = () => {};
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const url = new URL(request.url);
+  const pluginUrl = url.searchParams.get("pluginUrl");
+
+  if (!pluginUrl) {
+    return Response.json({ error: "Plugin URL is required" }, { status: 400 });
+  }
+
+  // Define where to store the downloaded zip file temporarily
+  const tempFile = path.join(__dirname, "../../../public/temp/plugin.zip");
+};
